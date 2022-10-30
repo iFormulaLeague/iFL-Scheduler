3082ba8fbc7995ce1ad88b970415cbb80380b763
jbies121 <15245768+jbies121@users.noreply.github.com>
2022-10-30 14:53:05 -0500

    Create auth_gcal to be called by gcal methods

--- layer/shared/schedule.py
+++ layer/shared/schedule.py
@@ -73,29 +73,9 @@ class Schedule:
         return
 
     def get_gcal_events(self):
-        # Prints the start and name of the season's events on the iFL AM calendar.
-        creds = None
-        self.seasonstart = self.event_info[0][0]
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file(
-                'token.json', self.SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
+        self.auth_gcal()
         try:
-            service = build('calendar', 'v3', credentials=creds)
+            service = build('calendar', 'v3', credentials=self.creds)
             # Call the Calendar API
             # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
             seasonstart = datetime.datetime.isoformat(self.seasonstart) + 'Z'
@@ -117,6 +97,30 @@ class Schedule:
 
         except HttpError as error:
             print('An error occurred: %s' % error)
+
+    def auth_gcal(self):
+        # Prints the start and name of the season's events on the iFL AM calendar.
+        creds = None
+        self.seasonstart = self.event_info[0][0]
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file(
+                'token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        self.creds = creds
 
 
 class Updater:
